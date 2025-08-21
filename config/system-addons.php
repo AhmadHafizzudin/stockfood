f00af3291d6affae56966885fa4414c7efa69681
Hafiz <ahmadhafizzudin22@gmail.com>
2025-08-21 17:03:49 +0800

    first push

---
+++ config/system-addons.php
@@ -0,0 +1,56 @@
+<?php return array (
+  'admin_panel' => 
+  array (
+    'active' => '1',
+    'username' => 'Hafiz',
+    'purchase_key' => '19f8a2b7-ca5c-4f7d-92b4-3e8c7d2a19a1',
+    'software_id' => 'MzM1NzE3NTA=',
+    'domain' => 'stackfood.test',
+    'software_type' => 'product',
+  ),
+  'vendor_panel' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'user_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'restaurant_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'deliveryman_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'react_web' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+);
