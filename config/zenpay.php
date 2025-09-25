f383777b951645bed885ed0c9cdd839aebc00851
airil <airilnlmoksin@gmail.com>
2025-09-25 18:03:03 +0800

    fix

--- config/zenpay.php
+++ config/zenpay.php
@@ -3,7 +3,7 @@ return [
     'base_url' => env('ZENPAY_BASE_URL'),
     'biller_code' => env('ZENPAY_BILLER_CODE'),
     'api_key' => env('ZENPAY_API_KEY'),
-    'api_secret' => env('ZENPAY_API_SECRET'),
+    'api_secret' => env('ZENPAY_SECRET_KEY'),
     'callback_url' => env('ZENPAY_CALLBACK_URL'),
     'return_url' => env('ZENPAY_RETURN_URL'),
     'decline_url' => env('ZENPAY_DECLINE_URL'),
